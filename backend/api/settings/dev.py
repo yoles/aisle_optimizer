5c353b5edf673fd930eb1123ddd5260e396bc676
yoles <yo-0ne@hotmail.fr>
2024-01-23 18:46:26 +0100

    Reformat with black

--- backend/api/settings/dev.py
+++ backend/api/settings/dev.py
@@ -1,6 +1,6 @@
-from api.settings.base import * # noqa
+from api.settings.base import *  # noqa
 
-ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1', "api"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "api"]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost/",
@@ -10,11 +10,15 @@ CORS_ALLOWED_ORIGINS = [
 
 # https://testdriven.io/blog/django-spa-auth/
 # https://parthkoshta.medium.com/dockerize-your-react-django-rest-api-application-and-serve-using-nginx-6f9ccf17105b
-CSRF_COOKIE_SAMESITE = 'Strict'
-SESSION_COOKIE_SAMESITE = 'Strict'
+CSRF_COOKIE_SAMESITE = "Strict"
+SESSION_COOKIE_SAMESITE = "Strict"
 CSRF_COOKIE_HTTPONLY = True
 SESSION_COOKIE_HTTPONLY = True
-CSRF_TRUSTED_ORIGINS = ['http://localhost', 'http://127.0.0.1', "http://0.0.0.0"]
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost",
+    "http://127.0.0.1",
+    "http://0.0.0.0",
+]
 
 # PROD ONLY
 # CSRF_COOKIE_SECURE = True
