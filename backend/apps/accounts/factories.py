045f9a16656aac1779bbea4b680e097cc6bbb0ab
yoles <yo-0ne@hotmail.fr>
2024-01-30 07:41:38 +0100

    Update(user): Add password in user factory
    Add hashed password on user factory to test login view

--- backend/apps/accounts/factories.py
+++ backend/apps/accounts/factories.py
@@ -6,9 +6,13 @@ from .models import User
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
+        exclude = ("password",)
 
     first_name = factory.Faker("first_name")
     last_name = factory.Faker("last_name")
+    password = factory.PostGenerationMethodCall(
+        "set_password", "default_password"
+    )
     email = factory.LazyAttribute(
         lambda obj: f"{obj.first_name[0]}.{obj.last_name}@example.com"
     )
